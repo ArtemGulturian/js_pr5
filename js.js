daa122b36e692aa20dcd3a781da5f6bcebc13c50
art <gulturyan.a@gmail.com>
2022-06-20 14:14:34 +0300

    add home work

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 "use srtict";
-
+/*
 const usdCurr = 28;
 const eurCurr = 32;
 const discount = 0.9;
@@ -15,8 +15,8 @@ function promotion(result){
 const res = convert(500, usdCurr);
 promotion(res);
 
-/*convert(500, usdCurr);
-convert(500, eurCurr);*/
+convert(500, usdCurr);
+convert(500, eurCurr);
 
 //-----
 
@@ -32,3 +32,36 @@ test();
 
 function doNothing(){}
 console.log(doNothing() === undefined);
+*/
+//-------------------------------
+
+// Место для первой задачи
+function sayHello(name) {
+    return `Привет, ${name}!`;
+}
+console.log(sayHello('Антон'));
+
+// Место для второй задачи
+function returnNeighboringNumbers(num) {
+    return [num - 1, num, num +1];
+}
+console.log(returnNeighboringNumbers(5));
+
+// Место для третьей задачи
+function getMathResult(num1, num2) {
+    if (typeof(num2) !== 'number' || num2 <= 0){
+        return num1;
+    }
+    
+    let str = '';
+
+    for (let i = 1; i <= num2; i++){
+        if (i === num2){
+            str += `${num1 * i}`;
+    } else {
+        str += `${num1 * i}---`;
+    }
+    }
+    return str;
+}
+console.log(getMathResult(10, 5));
