51cc18f41dbceb471498b58cfd8bc9411c669fcc
art <gulturyan.a@gmail.com>
2022-06-20 11:58:16 +0300

    add function convert

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 "use strict";
-
-function showFirstMessage(){
-    console.log("Hello World!");
+/*
+function showFirstMessage(text) {
+    console.log(text);
 }
 
-showFirstMessage();
+showFirstMessage("Hello World!");
+
+function calc(a,b) {
+    return(a + b);
+}
+
+console.log(calc(4,3));
+console.log(calc(5,6));
+console.log(calc(10,6));
+
+//------------------------
+
+function ret(){
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+*/
+//-----------------
+
+const logger = function(){
+    console.log("Hello");
+};
+
+logger();
+
+//const calc = (a, b) => a + b;
+
+const calc = (a, b) => {
+    console.log('1');
+    return a + b;
+}
